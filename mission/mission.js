beb2eab24da33eb375c67dc772e9c304f6030413
BrackenHibbert <hib23001@byui.edu>
2025-01-18 11:38:59 -0700

    Finished up week 2 assignment by adding in a selectable theme of light or dark

--- mission/mission.js
+++ mission/mission.js
@@ -1,4 +1,17 @@
 const themeSelector = document.querySelector('#theme');
 function changeTheme() {
-    
+    const currentTheme = themeSelector.value;
+    const body = document.body;
+    const lofo = document.getElementById('logo');
+
+    if (currentTheme === 'dark') {
+        body.classList.add('dark');
+        logo.src = 'byui-logo_white.png';
+    }
+    else {
+        body.classList.remove('dark');
+        logo.src = 'byui-logo_blue.webp';
+    }
 }
+
+themeSelector.addEventListener('change', changeTheme);
