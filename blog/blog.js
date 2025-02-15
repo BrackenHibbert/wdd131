104535eb710e5f68b2b322eccd94589c30394963
BrackenHibbert <hib23001@byui.edu>
2025-02-15 12:17:55 -0700

    finished up the js for the blog page

--- blog/blog.js
+++ blog/blog.js
@@ -23,5 +23,60 @@ const articles = [
 		ages: '12-16',
 		genre: 'Fantasy',
 		stars: '⭐⭐⭐⭐'
+	},
+	{
+		id: 3,
+		title: "Belgariad Book One: Pawn of Prophecy",
+		date: "Feb 12, 2022",
+		description:
+		"A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his 'Aunt Pol' and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.",
+		imgSrc:
+		"https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg",
+		imgAlt: "Book cover for Pawn of Prophecy",
+		ages: "12-16",
+		genre: "Fantasy",
+		stars: "⭐⭐⭐⭐⭐"
 	}
 ]
+
+
+function createArticles() {
+	console.log('Creating articles');
+	const articlesContainer = document.querySelector('.book');
+	console.log('articles container:', articlesContainer);
+
+	const column3 = articlesContainer.querySelector('.column3');
+
+	articlesContainer.innerHTML = '';
+	articlesContainer.appendChild(column3);
+	articles.forEach(article => {
+		console.log('processing article:', article);
+
+		const articleElement = document.createElement('article');
+
+		const column1 = `
+			<section class="column1">
+				<h1>${article.date}</h1>
+				<p>${article.ages}</p>
+				<p>${article.genre}</p>
+				<p>${article.stars}</p>
+			</section>
+		`;
+
+		const column2 = `
+			<section class="column2">
+				<h1>${article.title}</h1>
+				<img src="${article.imgSrc}" alt="Picture of Book Cover">
+				<p>${article.description}</p>
+			</section>
+		`;
+
+		articleElement.innerHTML = column1 + column2;
+		articlesContainer.appendChild(articleElement);
+	});
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+	console.log('DOM fully loaded and parsed');
+	createArticles();
+});
